523054fad30aa74083a4188e95ea6dc02aa5b2fb
BotMos <botmosorg@users.noreply.github.com>
2022-04-23 17:31:47 +0200

    Start writing the manifest

---
+++ characters.js
@@ -0,0 +1,28 @@
+NAMES = {
+    "BotMoses": "Teaches the new player",
+    "Betty": "Gambles a lot",
+    "Bender": "From Futurama, round head, bowl head",
+    // ...
+}
+
+// It's a little bit like PacMan, spirits jump into the player.
+SPIRITS = {
+    "WorkBot": "Basic pawn",
+    "AeroBot": "Basic server, serving goo/oil and such to bots",
+    // ...
+    "RichBot": "Shiny metal ass",
+    // ...
+    "PirateBot": "Needs to be unlocked, achieve ship mastery",
+}
+
+/*
+* Spawns a new bot from one of the selected spirits, after player death
+*/
+MAX_NUMBER_PLAYERS = 1000000000; // Placeholder
+function spawn(spirit, mapId) {
+    return {
+        "ID": Math.floor(Math.random() * MAX_NUMBER_PLAYERS),
+        "spirit": spirit,
+        "mapId": mapId,
+    };
+}
---
+++ maps.js
@@ -0,0 +1,2 @@
+MAX_MAP_SIZE = 65536; // Should be enough space for a map in a 2D roguelike
+
