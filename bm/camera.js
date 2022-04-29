fe097b4cd2c66c20cb36379cab43ec9c865e6683
BotMos <botmosorg@users.noreply.github.com>
2022-04-29 21:41:14 +0200

    Fullscreen camera

--- bm/camera.js
+++ bm/camera.js
@@ -1,3 +1,9 @@
 
-const CAMERA_SIZE = [60, 34]
+const WINDOW_WIDTH_IN_PX = window.innerWidth
+const WINDOW_HEIGHT_IN_PX = window.innerHeight
 const FONT_SIZE = 18
+const CAMERA_PADDING = 1
+const CAMERA_SIZE = [
+    Math.floor(WINDOW_WIDTH_IN_PX / 18) - CAMERA_PADDING,
+    Math.floor(WINDOW_HEIGHT_IN_PX / 18) - CAMERA_PADDING - 1 // Minus another 1 due to status line
+]
