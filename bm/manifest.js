38b54a86bf7db9da4ba56a5c9d79d0f40371eaa9
BotMos <botmosorg@users.noreply.github.com>
2022-04-30 11:33:35 +0200

    Add worldobjects to manifest

--- bm/manifest.js
+++ bm/manifest.js
@@ -11,19 +11,14 @@ class Spirit {
         this.icon = icon;
         this.unlockCondition = null;
     }
-    static SPIRITS = [
-        new Spirit("Spirit", "You are back in the machine mind, pick a new hull!", "@"),
-        new Spirit("WorkBot", "Basic pawn", "B"),
-        new Spirit("AeroBot", "Basic server, serving goo/oil and such to bots", "A"),
-        /*
-        "PioneerBot": "Absolute slayer bots, needs to play as WorkBot first, brotherhood of ultimate warriors",
-        "ValkyrBot": "Female Godess from the sky, needs to play as AeroBot first",
-        // ...
-        "RichBot": "Shiny metal ass, owns a spacestation, is the brain so to say",
-        // ...
-        "PirateBot": "Needs to be unlocked, achieve ship mastery",
-        */
-    ];
+}
+class WorldObject {
+    constructor(name, description, icon, color) {
+        this.name = name;
+        this.description = description;
+        this.icon = icon;
+        this.color = color;
+    }
 }
 
 const MANIFEST = {
@@ -31,5 +26,18 @@ const MANIFEST = {
         "Spirit":  new Spirit("Spirit", "You are back in the machine mind, pick a new hull!", "@"),
         "WorkBot": new Spirit("WorkBot", "Basic pawn", "B"),
         "AeroBot": new Spirit("AeroBot", "Basic server, serving goo/oil and such to bots", "A"),
+           /*
+        "PioneerBot": "Absolute slayer bots, needs to play as WorkBot first, brotherhood of ultimate warriors",
+        "ValkyrBot": "Female Godess from the sky, needs to play as AeroBot first",
+        // ...
+        "RichBot": "Shiny metal ass, owns a spacestation, is the brain so to say",
+        // ...
+        "PirateBot": "Needs to be unlocked, achieve ship mastery",
+        */
+    },
+    "objects": {
+        "boulder": new WorldObject("boulder", "A movable rock", "b", "#777"),
+        "chest": new WorldObject("chest", "A chest. What might be inside?", "c", "goldenrod"),
+        "log": new WorldObject("log", "A movable tree trunk", "l", "brown"),
     }
 }
