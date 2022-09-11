ddbd1b53aec33bd8cd09b13eddd2e7697c0355d0
BotMos <botmosorg@users.noreply.github.com>
2022-09-11 22:36:55 +0200

    Back to smaller font

--- bm/config.js
+++ bm/config.js
@@ -12,7 +12,7 @@ export const SHOW_GRID = true
 
 const WINDOW_WIDTH_IN_PX = window.innerWidth
 const WINDOW_HEIGHT_IN_PX = window.innerHeight
-export const FONT_SIZE = 24
+export const FONT_SIZE = 18
 const CAMERA_PADDING = 1
 export const CAMERA_SIZE = [
     Math.floor(WINDOW_WIDTH_IN_PX / 18) - CAMERA_PADDING,
