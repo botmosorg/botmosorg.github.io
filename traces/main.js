c337af83a066f14c923ca2dca478fdbd7fb5c516
BotMos <botmosorg@users.noreply.github.com>
2022-09-29 17:29:57 +0200

    Cleanup

--- traces/main.js
+++ traces/main.js
@@ -2,10 +2,6 @@
 
 import Game from "./game.js";
 
-//const UPDATE_EVERY = 500 // in ms
-//const UPDATE_EVERY = 50 // in ms
-const UPDATE_EVERY = 100 // in ms
-
 let game = new Game();
 window.onload = game.init();
 
