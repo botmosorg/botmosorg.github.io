e46695674581bf86c6ffd8e558590ab801eadbd8
BotMos <botmosorg@users.noreply.github.com>
2022-09-29 14:57:56 +0200

    Option class

--- traces/rooms.js
+++ traces/rooms.js
@@ -8,6 +8,16 @@ class Room {
     }
 }
 
+class Option {
+    constructor(intros, down=null, left=null, right=null, up=null) {
+        this.intros = intros;
+        this.down = down;
+        this.left = left;
+        this.right = right;
+        this.up = up;
+    }
+}
+
 export const ROOMS = {
     "Pod": new Room("Pod", ["Standard bot storage pod, nothing here really."], ["Hallway"]),
     "Hallway": new Room("Hallway", ["The hallway, lots of bots on their way to work."], ["Pod"])
