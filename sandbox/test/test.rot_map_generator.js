fdc8b080c27c6a410a35171449ffcf25a90ef5e2
BotMos <botmosorg@users.noreply.github.com>
2023-06-09 22:04:46 +0200

    Extend assertions

--- sandbox/test/test.rot_map_generator.js
+++ sandbox/test/test.rot_map_generator.js
@@ -11,5 +11,9 @@ describe('maps_create_overworld', function () {
         expect(overworld.id).to.equal("simplex=1337");
         expect(overworld.widthTiles).to.equal(256);
         expect(overworld.heightTiles).to.equal(256);
+        expect(overworld.getTile(127, 127).type.name).to.equal("tree");
+        expect(overworld.getTile(127, 128).type.name).to.equal("void");
+        expect(overworld.getTile(126, 121).type.name).to.equal("portal");
+        expect(overworld.getTile(135, 127).type.name).to.equal("water");
     });
 });
