63bd3458931067ca5733ace28ff1fc093f9448ec
BotMos <botmosorg@users.noreply.github.com>
2023-06-12 22:49:39 +0200

    Comments

--- sandbox/src/combat.js
+++ sandbox/src/combat.js
@@ -11,7 +11,7 @@ export function combat_queue(entityId, otherEntityId) {
 export function combat_update() {
     let combatants = undefined
     while (typeof(combatants = _combatQueue.shift()) !== 'undefined') {
-        energy_queue(combatants[0], -1)
-        energy_queue(combatants[1], -10)
+        energy_queue(combatants[0], -1)  // Energy cost of attack
+        energy_queue(combatants[1], -10) // Energy damage done
     }
 }
