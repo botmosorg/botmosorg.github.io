1aa6c8c3f54aba2d8f0b8a5c28f2a33d878e9672
BotMos <botmosorg@users.noreply.github.com>
2023-12-16 03:05:43 +0100

    Move map population code to map generators, from game init

--- sandbox/src/game.ts
+++ sandbox/src/game.ts
@@ -26,18 +26,6 @@ export default class Game {
             this.state.maps[mapId] = maps_parse(MANIFEST.maps[mapId])
         }
 
-        this.state = entities_create(this.state, "npc0", MANIFEST.spirits.AeroBot, "simplex="+MAP_SEED, 130, 127, {faction: MANIFEST.factions.Spirits})
-        this.state = entities_create(this.state, "npc1", MANIFEST.spirits.WorkBot, "simplex="+MAP_SEED, 124, 127, {faction: MANIFEST.factions.Spirits})
-        this.state = items_create(this.state, MANIFEST.items.energy, "simplex="+MAP_SEED, 127, 130)
-
-        this.state = entities_create(this.state, "enemy0", MANIFEST.spirits.WorkBot, "arena", 8, 8, {faction: MANIFEST.factions.Pyrates})
-        this.state = entities_create(this.state, "enemy1", MANIFEST.spirits.WorkBot, "arena", 9, 8, {faction: MANIFEST.factions.Pyrates})
-        this.state = entities_create(this.state, "enemy2", MANIFEST.spirits.WorkBot, "arena", 11, 11, {faction: MANIFEST.factions.Pyrates})
-        this.state = entities_create(this.state, "enemy3", MANIFEST.spirits.WorkBot, "arena", 6, 6, {faction: MANIFEST.factions.Pyrates})
-        this.state = entities_create(this.state, "enemy4", MANIFEST.spirits.WorkBot, "arena", 12, 12, {faction: MANIFEST.factions.Pyrates})
-        this.state = items_create(this.state, MANIFEST.items.energy, "arena", 7, 7)
-        this.state = items_create(this.state, MANIFEST.items.energy, "arena", 14, 14)
-
         this.state.currentMapId = "preloader"
         this.state = entities_create(this.state, players_get_current(), MANIFEST.spirits.Spirit, this.state.currentMapId, 7, 7, {faction: MANIFEST.factions.Spirits})
 
--- sandbox/src/rot_map_generator.ts
+++ sandbox/src/rot_map_generator.ts
@@ -1,4 +1,6 @@
 import * as ROT from "../lib/rot.js"
+import { entities_create } from "./entity";
+import { items_create } from "./item";
 
 import { MANIFEST } from "./manifest";
 import { CHUNK_SIZE, Map, MAP_SIZE, tiles_create } from "./map";
@@ -46,6 +48,10 @@ export function maps_create_overworld(state: State, seed=MAP_SEED): State {
 
     state.maps[mapId] = map;
 
+    state = entities_create(state, "npc0", MANIFEST.spirits.AeroBot, "simplex="+MAP_SEED, 130, 127, {faction: MANIFEST.factions.Spirits})
+    state = entities_create(state, "npc1", MANIFEST.spirits.WorkBot, "simplex="+MAP_SEED, 124, 127, {faction: MANIFEST.factions.Spirits})
+    state = items_create(state, MANIFEST.items.energy, "simplex="+MAP_SEED, 127, 130)
+
     return state;
 }
 
@@ -67,6 +73,14 @@ export function maps_create_arena(state: State): State {
     map.setTile(1, 0, MANIFEST.tiles.portal, {mapId: "preloader", x: 15, y: 2});
 
     state.maps["arena"] = map
+
+    state = entities_create(state, "enemy0", MANIFEST.spirits.WorkBot, "arena", 8, 8, {faction: MANIFEST.factions.Pyrates})
+    state = entities_create(state, "enemy1", MANIFEST.spirits.WorkBot, "arena", 9, 8, {faction: MANIFEST.factions.Pyrates})
+    state = entities_create(state, "enemy2", MANIFEST.spirits.WorkBot, "arena", 11, 11, {faction: MANIFEST.factions.Pyrates})
+    state = entities_create(state, "enemy3", MANIFEST.spirits.WorkBot, "arena", 6, 6, {faction: MANIFEST.factions.Pyrates})
+    state = entities_create(state, "enemy4", MANIFEST.spirits.WorkBot, "arena", 12, 12, {faction: MANIFEST.factions.Pyrates})
+    state = items_create(state, MANIFEST.items.energy, "arena", 7, 7)
+    state = items_create(state, MANIFEST.items.energy, "arena", 14, 14)
 
     return state;
 }
