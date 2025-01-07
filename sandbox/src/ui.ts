ebbb6dd3e50a68fea106bc37b6eb24caad4b3975
BotMos <botmosorg@users.noreply.github.com>
2025-01-07 23:42:40 +0100

    Render Game over! in status line

--- sandbox/src/ui.ts
+++ sandbox/src/ui.ts
@@ -9,11 +9,16 @@ UI_ELEMENT.id = "ui"
 document.body.appendChild(UI_ELEMENT)
 
 export async function drawUI(state: State, cameraY: number=0) { // dirty hack: UI should not rely on camera
+    if (!BOTMOS_OPTIONS.showUI) {
+        return
+    }
+
     const playerId = players_get_current_id()
     const playerEntity = state.entities[playerId]
 
     // Render status line
-    if (!!playerEntity && BOTMOS_OPTIONS.showUI) {
+    const UI_LINE = document.createElement("span")
+    if (!!playerEntity) {
         let equippedItemText = ''
         const equippedItem = items_get_equipped(state, playerId)
         if (!!equippedItem) {
@@ -41,11 +46,8 @@ export async function drawUI(state: State, cameraY: number=0) { // dirty hack: U
             UI_ELEMENT.style.flexDirection = "column"
         }
 
-        const UI_LINE = document.createElement("span")
         UI_LINE.innerText = line
         UI_LINE.style.background = "#000"
-        UI_ELEMENT.replaceChildren(UI_LINE)
-
         // Red border when low energy
         if (playerEntity.energy / playerEntity.energyMax <= 0.2) {
             UI_LINE.style.color = "#000"
@@ -54,15 +56,21 @@ export async function drawUI(state: State, cameraY: number=0) { // dirty hack: U
             UI_LINE.style.color = "#74ee15"
             UI_LINE.style.background = "#000"
         }
+    } else {
+        UI_LINE.innerText = "Game over!"
+        UI_LINE.style.color = "#000"
+        UI_LINE.style.background = "#f00"
+    }
 
-        // Render menu / message log
-        if (state._menuOpen && !!state.chatLog.length) {
-            const lines = state.chatLog.slice(-BOTMOS_OPTIONS.chatLogMaxDisplaySize)
+    UI_ELEMENT.replaceChildren(UI_LINE)
 
-            const UI_CHATLOG_ELEMENT = document.createElement("div")
-            UI_CHATLOG_ELEMENT.id = "uichatlog"
-            UI_CHATLOG_ELEMENT.innerText = lines.join("\n")
-            UI_ELEMENT.appendChild(UI_CHATLOG_ELEMENT)
-        }
+    // Render menu / message log
+    if (state._menuOpen && !!state.chatLog.length) {
+        const lines = state.chatLog.slice(-BOTMOS_OPTIONS.chatLogMaxDisplaySize)
+
+        const UI_CHATLOG_ELEMENT = document.createElement("div")
+        UI_CHATLOG_ELEMENT.id = "uichatlog"
+        UI_CHATLOG_ELEMENT.innerText = lines.join("\n")
+        UI_ELEMENT.appendChild(UI_CHATLOG_ELEMENT)
     }
 }
