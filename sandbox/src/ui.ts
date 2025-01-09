ab1488637a1347729f36c7503290ff020451d54c
BotMos <botmosorg@users.noreply.github.com>
2025-01-09 02:12:48 +0100

    Optimize UI rendering

--- sandbox/src/ui.ts
+++ sandbox/src/ui.ts
@@ -8,6 +8,17 @@ import { State } from "./state"
 const UI_ELEMENT = document.createElement("div")
 UI_ELEMENT.id = "ui"
 document.body.appendChild(UI_ELEMENT)
+const UI_HULL_ELEMENT = _create_sprite_element()
+const UI_TOOL_ELEMENT = _create_sprite_element()
+const UI_LINE_TEXT_ELEMENT = _create_text_element()
+
+const UI_LINE = document.createElement("div")
+UI_LINE.replaceChildren(UI_HULL_ELEMENT, UI_TOOL_ELEMENT, UI_LINE_TEXT_ELEMENT)
+UI_ELEMENT.replaceChildren(UI_LINE)
+
+const UI_CHATLOG_ELEMENT = document.createElement("div")
+UI_CHATLOG_ELEMENT.id = "uichatlog"
+UI_ELEMENT.appendChild(UI_CHATLOG_ELEMENT)
 
 export async function drawUI(state: State, cameraY: number=0) { // dirty hack: UI should not rely on camera
     if (!BOTMOS_OPTIONS.showUI) {
@@ -18,17 +29,16 @@ export async function drawUI(state: State, cameraY: number=0) { // dirty hack: U
     const playerEntity = state.entities[playerId]
 
     // Render status line
-    const UI_LINE = document.createElement("div")
     let uiLineText = ''
-    const uiLineChildren = []
     if (!!playerEntity) {
-        uiLineChildren.push(_create_sprite_element(playerEntity.type.name))
+        _set_sprite(UI_HULL_ELEMENT, playerEntity.type.name)
 
         let equippedItemText = ''
         const equippedItem = items_get_equipped(state, playerId)
         if (!!equippedItem) {
-            //equippedItemText = equippedItem.type.name + ' '
-            uiLineChildren.push(_create_sprite_element(equippedItem.type.name.toLowerCase()))
+            _set_sprite(UI_TOOL_ELEMENT, equippedItem.type.name.toLowerCase())
+        } else {
+            _set_sprite(UI_TOOL_ELEMENT, null)
         }
 
         let matterText = ''
@@ -63,42 +73,48 @@ export async function drawUI(state: State, cameraY: number=0) { // dirty hack: U
         }
 
     } else {
+        _set_sprite(UI_HULL_ELEMENT, null)
+        _set_sprite(UI_TOOL_ELEMENT, null)
+
         uiLineText = "Game over!"
         UI_LINE.style.color = "#000"
         UI_LINE.style.background = "#f00"
     }
 
-    //UI_LINE.appendChild()
-    //UI_LINE.appendChild()
-    uiLineChildren.push(_create_text_element(uiLineText))
-    UI_LINE.replaceChildren(...uiLineChildren)
-    UI_ELEMENT.replaceChildren(UI_LINE)
+    UI_LINE_TEXT_ELEMENT.innerText = uiLineText
 
     // Render menu / message log
     if (state._menuOpen && !!state.chatLog.length) {
         const lines = state.chatLog.slice(-BOTMOS_OPTIONS.chatLogMaxDisplaySize)
 
-        const UI_CHATLOG_ELEMENT = document.createElement("div")
-        UI_CHATLOG_ELEMENT.id = "uichatlog"
         UI_CHATLOG_ELEMENT.innerText = lines.join("\n")
-        UI_ELEMENT.appendChild(UI_CHATLOG_ELEMENT)
+    } else {
+        UI_CHATLOG_ELEMENT.innerText = ''
     }
 }
 
-function _create_sprite_element(spriteId: string): HTMLElement {
+function _create_sprite_element(): HTMLElement {
     const uiSprite = document.createElement("span")
-    const offset = TILEMAP[spriteId][0]
-    uiSprite.style.background = `url('build/tiles.png') -${offset}px 0`
-    uiSprite.style.width = `${BOTMOS_OPTIONS.fontSize}px`
+    _set_sprite(uiSprite, null)
+
     uiSprite.style.height = `${BOTMOS_OPTIONS.fontSize}px`
     uiSprite.style.display = "inline-block"
     return uiSprite
 }
 
-function _create_text_element(text: string): HTMLElement {
+function _create_text_element(): HTMLElement {
     const textElement = document.createElement("span")
     textElement.style.display = "inline-block"
     textElement.style.marginLeft = "8px"
-    textElement.innerText = text
     return textElement
+}
+
+function _set_sprite(element: HTMLElement, spriteId: string | null) {
+    if (!!spriteId) {
+        const offset = TILEMAP[spriteId][0]
+        element.style.background = `url('build/tiles.png') -${offset}px 0`
+        element.style.width = `${BOTMOS_OPTIONS.fontSize}px`
+    } else {
+        element.style.width = `0px`
+    }
 }
